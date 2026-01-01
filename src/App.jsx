c55fb5e0695129b7610f228d0a495b1b67694c82
Piyush Hadiya <hadiyapiyush07@gmail.com>
2026-01-01 16:34:47 +0530

    changes and commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='h-screen lg:flex bg-black text-white'>
+      <form onSubmit={(e)=>
+        {submitHandler(e)}
+      } className='flex gap-4 p-10 lg:w-1/2 flex-col items-start'>
+          <h1 className='text-4xl font-bold'>Add Notes</h1>
+
+
+          <input 
+        type="text" 
+        placeholder="Enter Notes Heading" 
+        className='px-5 w-full font-medium py-2 border-2 outline-none rounded'
+        />
+        <textarea 
+        type="text" 
+        className='px-5 w-full h-30 flex font-medium flex-row items-start py-2 border-2 outline-none rounded'
+        placeholder='Write Details'
+        />
+        <button className='bg-white w-full font-medium text-black px-5 py-2 outline-none rounded'>Add Notes</button>
+      </form>
+      <div className='lg:w-1/2 lg:border-l-2 p-10'>
+      <h1 className='text-4xl font-bold'>Recent Notes</h1>
+       <div className='flex gap-5 flex-wrap h-full mt-5 overflow-auto'>
+        <div className="h-52 w-40 rounded-2xl bg-white"></div>
+        <div className="h-52 w-40 rounded-2xl bg-white"></div>
+       </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
